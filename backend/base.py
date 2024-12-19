9e639a1dfc6d858932df42598d397384d88ac522
ShashaankS <shashaank.srivastava04@gmail.com>
2024-12-19 15:17:24 +0530

    `Removed unused files and updated dependencies`

--- backend/base.py
+++ backend/base.py
@@ -8,19 +8,18 @@ from bs4 import BeautifulSoup
 app = Flask(__name__)
 CORS(app)
 
-# Initialize the summarizer pipeline
 summarizer = pipeline("summarization", model="facebook/bart-base")
 
 def extract_article_text(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
-    article_body = soup.find('article')  # Adjust the tag as needed
+    article_body = soup.find('article')
     article_text = article_body.get_text(separator="\n", strip=True)
     return article_text
 
 def summarize_text(text):
     if len(text) > 1000:
-        text = text[:1000]  # Truncate to 1000 characters
+        text = text[:1000]
     summary = summarizer(text, max_length=150, min_length=30, length_penalty=2.0)
     return summary[0]['summary_text']
 
